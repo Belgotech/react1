2fb77a278ea616e122b80d67908c6f76c2b3dafa
belgotech <belovedobiora2002@gmail.com>
2023-07-01 22:29:46 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
+  const handleCount = () => {
+    setCount(count + 1)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          {count}
+        </h3>
+        <button onClick={handleCount}>
+          click to add {count}
+        </button>
       </header>
     </div>
   );
