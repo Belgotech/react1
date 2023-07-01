10bd4e17398d8fd52c17fb2b894e3c18777a8c5d
belgotech <belovedobiora2002@gmail.com>
2023-07-01 22:52:37 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import { useState, useEffect } from "react";
 function App() {
 
   const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
 
   const handleCount = () => {
     setCount(count + 1)
   }
+  const handleCount2 = () => {
+    setCount2(count2 + 1)
+  }
 
   useEffect(()=>{
-    document.title=`${count} new message!`
-  })
+    document.title=`${count} new message and ${count2} calls!`
+  },)
 
   return (
     <div className="App">
@@ -20,8 +24,14 @@ function App() {
         <h3>
           {count} new message
         </h3>
+        <h3>
+          {count2} new calls
+        </h3>
         <button onClick={handleCount}>
           click to add {count}
+        </button>
+        <button onClick={handleCount2}>
+          click to add {count2}
         </button>
       </header>
     </div>
